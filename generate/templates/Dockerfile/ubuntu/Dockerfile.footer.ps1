5ddafb888007282b5821e39ccb2724c2c9bd50e3
Leo <leonardjonathanoh@live.com>
2019-01-24 23:11:07 +0800

    update STOPSIGNAL for entrypoint so that stopping container works properly

--- generate/templates/Dockerfile/ubuntu/Dockerfile.footer.ps1
+++ generate/templates/Dockerfile/ubuntu/Dockerfile.footer.ps1
@@ -2,6 +2,9 @@
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
 
+# This is not nice, but unfortunately TERM does not work
+STOPSIGNAL SIGKILL
+
 ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["varnish-agent", "-d"]
 "@
