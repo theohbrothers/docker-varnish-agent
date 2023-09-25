19ebb29c16193361bf5da7f89957403a78e8cc15
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-25 16:14:06 +0000

    Enhancement (ci): Add support for incremental variants

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -1,16 +1,22 @@
+$local:VERSIONS = @(
+    '4.1.4'
+)
 # Docker image variants' definitions
 $VARIANTS = @(
-    @{
-        _metadata = @{
-            components = @( 'varnishdashboard' )
-            platforms = 'linux/386,linux/amd64,linux/arm/v7,linux/arm64,linux/s390x'
-            VARNISH_AGENT_VERSION = '4.1.4'
-            VARNISH_DASHBOARD_COMMIT = "e2cc1c854941c9fac18bdfedba2819fa766a5549"
-            # distro = 'ubuntu'
-            # distro_version = '16.04'
+    foreach ($v in $local:VERSIONS) {
+        @{
+            _metadata = @{
+                components = @( 'varnishdashboard' )
+                platforms = 'linux/386,linux/amd64,linux/arm/v7,linux/arm64,linux/s390x'
+                VARNISH_AGENT_VERSION = $v
+                VARNISH_DASHBOARD_COMMIT = "e2cc1c854941c9fac18bdfedba2819fa766a5549"
+                # distro = 'ubuntu'
+                # distro_version = '16.04'
+                job_group_key = $v
+            }
+            tag = "$v-ubuntu-16.04"
+            tag_as_latest = $true
         }
-        tag = '4.1.4-ubuntu-16.04'
-        tag_as_latest = $true
     }
 )
 
